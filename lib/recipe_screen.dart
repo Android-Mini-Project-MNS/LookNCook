723e7ecf6b34201e3d1bb896ec919a58ace4d116
Nandana Santhosh <nandanapsmail@gmail.com>
2024-06-09 21:47:18 +0530

    added items in an basket

--- lib/recipe_screen.dart
+++ lib/recipe_screen.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:autocomplete_textfield/autocomplete_textfield.dart';
 
-
 class RecipeScreen extends StatefulWidget {
   const RecipeScreen({Key? key}) : super(key: key);
 
@@ -15,6 +14,7 @@ class _RecipeScreenState extends State<RecipeScreen> {
   final GlobalKey<AutoCompleteTextFieldState<String>> key = GlobalKey();
   TextEditingController _ingredientController = TextEditingController();
   List<String> _ingredients = [];
+  List<String> _allIngredients = ['Tomato', 'Onion', 'Garlic', 'Chicken']; // Example ingredient suggestions
   List<String> _recipes = [];
 
   @override
@@ -30,7 +30,7 @@ class _RecipeScreenState extends State<RecipeScreen> {
             AutoCompleteTextField<String>(
               key: key,
               clearOnSubmit: false,
-              suggestions: _ingredients,
+              suggestions: _allIngredients,
               decoration: InputDecoration(
                 labelText: 'Enter an ingredient',
               ),
@@ -56,16 +56,65 @@ class _RecipeScreenState extends State<RecipeScreen> {
               onPressed: _addIngredient,
               child: const Text('Add Ingredient'),
             ),
-            // Rest of your code...
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _getRecipes,
+              child: const Text('Get Recipes'),
+            ),
           ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 150,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/basket.png'), // Ensure this image exists in your assets
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: _ingredients.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Chip(
+                label: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(_ingredients[index]),
+                    GestureDetector(
+                      onTap: () => _removeIngredient(index),
+                      child: Icon(
+                        Icons.close,
+                        color: Colors.red,
+                        size: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
   }
+
   void _addIngredient() {
     setState(() {
-      _ingredients.add(_ingredientController.text);
+      if (_ingredientController.text.isNotEmpty && !_ingredients.contains(_ingredientController.text)) {
+        _ingredients.add(_ingredientController.text);
+        print('Ingredient added: ${_ingredientController.text}'); // Debug print statement
+      }
       _ingredientController.clear();
+    });
+  }
+
+  void _removeIngredient(int index) {
+    setState(() {
+      print('Ingredient removed: ${_ingredients[index]}'); // Debug print statement
+      _ingredients.removeAt(index);
     });
   }
 
@@ -87,5 +136,4 @@ class _RecipeScreenState extends State<RecipeScreen> {
       throw Exception('Failed to load recipes');
     }
   }
-
 }
